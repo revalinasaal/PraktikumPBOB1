3a6639f2999706eaea03eb2cccfcc33557384265
Revalina Salwa Aliya W.P <revalinasaal@gmail.com>
2026-04-05 13:06:23 +0700

    praktikum

---
+++ pertemuan5/src/IResize.java
@@ -0,0 +1,10 @@
+/* Nama File : IResize.java
+ * Deskripsi : interface untuk meresize objek
+ * Pembuat   : Revalina Salwa
+ * Tanggal   : 18-3-26
+*/
+
+public interface IResize{
+    void zoomIn();
+    void zoomOut();
+}
