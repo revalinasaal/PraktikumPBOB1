bd7912e9b2d10d62f9b06547f9193cb5f5b97b48
Revalina Salwa Aliya W.P <revalinasaal@gmail.com>
2026-05-05 21:45:04 +0700

    praktikum

---
+++ src/Kembangtelon.java
@@ -0,0 +1,21 @@
+/* Nama File : kwmbangtelon.java
+ * Deskripsi : Subkelas Kembangtelon yang merupakan jenis kucing dengan implementasi metode gerak() dan bersuara() sesuai karakteristik kucing Kembangtelon.
+ * Pembuat   : Revalina Salwa Aliya W.P / 24060124140155
+ * Tanggal   : 1-5-26
+*/
+
+public class Kembangtelon extends Kucing{
+    public Kembangtelon(String nama, double bobot){
+        super(nama, bobot);
+    }
+
+    @Override
+    public void gerak() {
+        System.out.println(nama + "berjalan santai");
+    }
+
+    @Override
+    public void bersuara() {
+        System.out.println(nama + "berbunyi meong keras");
+    }
+}
