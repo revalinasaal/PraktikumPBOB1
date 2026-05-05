25b5063029fbdd095b2f052c849dcb8fef1c292a
Revalina Salwa Aliya W.P <revalinasaal@gmail.com>
2026-05-05 21:47:10 +0700

    praktikum

---
+++ PBO08_24060124140155/src/Datum.java
@@ -0,0 +1,17 @@
+/* Nama File : Datum.java
+ * Deskripsi : Program untuk membuat kelas Datum yang dapat menyimpan satu elemen dengan tipe data generik, serta metode untuk mengakses dan memodifikasi elemen tersebut.
+ * Pembuat   : Revalina Salwa Aliya W.P / 24060124140155
+ * Tanggal   : 1-5-26
+*/
+
+public class Datum<T>{
+    private T isi;
+
+    public T getIsi(){
+        return isi;
+    }
+
+    public void setIsi(T isiBaru){
+        this.isi = isiBaru;
+    }
+}
